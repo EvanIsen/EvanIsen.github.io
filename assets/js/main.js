24385352b3c945e8ea3b05c128f5c8a853419fdf
Pouyoute <evan.mallevaey@isen.yncrea.fr>
2024-05-23 14:16:45 +0200

    feur

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,26 +224,8 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: true
     },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
